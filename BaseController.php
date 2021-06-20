20cf7de272f2d123ba038d10f8f7e5b65d4b8c57
basic-app <dev@basic-app.com>
2021-06-20 19:39:17 +0300

    n/a

--- BaseController.php
+++ BaseController.php
@@ -6,11 +6,26 @@
  */
 namespace BasicApp\Controller;
 
+use Psr\Log\LoggerInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
 abstract class BaseController extends \CodeIgniter\Controller
 {
 
     use ActionsTrait;
 
     use ControllerTrait;
+
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        parent::initController($request, $response, $logger);
+
+        $this->initialize();
+    }
+
+    protected function initialize()
+    {
+    }
 
 }
