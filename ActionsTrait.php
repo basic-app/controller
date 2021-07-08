3ee1915e55cb9fd85aeca29539dc9672fd3c67ef
basic-app <dev@basic-app.com>
2021-07-08 12:16:49 +0300

    n/a

--- ActionsTrait.php
+++ ActionsTrait.php
@@ -6,6 +6,7 @@
  */
 namespace BasicApp\Controller;
 
+use Exception;
 use Closure;
 use CodeIgniter\Exceptions\PageNotFoundException;
 use Webmozart\Assert\Assert;
@@ -20,37 +21,69 @@ trait ActionsTrait
 
     protected $allowedActions;
 
+    protected $_actions = [];
+
     protected function remapAction($method, ...$params)
     {
-        $actions = array_merge($this->defaultActions, $this->actions);
+        $action = $this->getAction($method);
 
-        if (array_key_exists($method, $actions) && $actions[$method])
+        if (!$this->beforeAction($method, $error))
         {
-            if (!$this->isActionAllowed($method, $error))
-            {
-                throw PageNotFoundException::forPageNotFound($error ?? lang('Page not found.'));
-            }
-
-            if (is_array($actions[$method]))
-            {
-                $action = call_user_func_array([$this, 'createAction'], $actions[$method]);
-            }
-            else
-            {
-                $action = $this->createAction($actions[$method]);
-            }
-
-            $action->initialize($method);
-
-            if (!$this->beforeAction($action, $error))
-            {
-                $this->throwSecurityException($error ?? lang('Access denied.'));
-            }
-
-            return $action->execute(...$params);
+            $this->throwSecurityException($error ?? lang('Access denied.'));
         }
 
-        throw PageNotFoundException::forPageNotFound(lang('Page not found.'));        
+        return $action->execute(...$params);
+    }
+
+    protected function getActionOrFail($method, $error = null) : ActionInterface
+    {
+        if (array_key_exists($method, $this->_actions))
+        {
+            return $this->_actions[$method];
+        }
+
+        throw PageNotFoundException::forPageNotFound($error ?? lang('Page not found.'));
+    }
+
+    protected function getAction(string $method, array $params = []) : ActionInterface
+    {
+        $actions = array_merge($this->defaultActions, $this->actions);
+        
+        if (!array_key_exists($method, $actions) || !$actions[$method])
+        {
+            throw PageNotFoundException::forPageNotFound(lang('Page not found.'));
+        }
+
+        if (!$this->isActionAllowed($method, $error))
+        {
+            throw PageNotFoundException::forPageNotFound($error ?? lang('Page not found.'));
+        }
+
+        if (is_array($actions[$method]))
+        {
+            Assert::arrayHasKey('class', $actions[$method]);
+
+            $class = $actions[$method]['class'];
+
+            unset($actions[$method]['class']);
+
+            $params = array_merge($actions[$method], $params);
+
+            $this->_actions[$method] = call_user_func_array([$this, 'createAction'], [$class, $params]);
+        }
+        else
+        {
+            $this->_actions[$method] = $this->createAction($actions[$method], $params);
+        }
+
+        /*
+        if (!$this->beforeAction($method, $error))
+        {
+            $this->throwSecurityException($error ?? lang('Access denied.'));
+        }
+        */
+        
+        return $this->_actions[$method];
     }
 
     protected function createAction(string $actionClass, array $params = [])
@@ -63,14 +96,16 @@ trait ActionsTrait
 
         $action->setResponse($this->response);
 
+        $action->initialize();
+
         return $action;
     }
 
-    protected function isActionAllowed(string $actionClass, &$error = null) : bool
+    protected function isActionAllowed(string $method, &$error = null) : bool
     {
         if ($this->allowedActions !== null)
         {
-            if (array_search($actionClass, $this->allowedActions) !== false)
+            if (array_search($method, $this->allowedActions) !== false)
             {
                 return true;
             }
@@ -83,7 +118,7 @@ trait ActionsTrait
         return true;
     }
 
-    protected function beforeAction(ActionInterface $action, &$error = null) : bool
+    protected function beforeAction(string $action, &$error = null) : bool
     {
         return true;
     }
